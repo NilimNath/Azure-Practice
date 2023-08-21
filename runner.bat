0cfd863d359327b49ce0f5bb89064f1515efcbad
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2023-08-21 15:25:41 +1000

    Update and rename test-runner.bat to runner.bat

---
+++ runner.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" ""C:\Users\nilim.nath\OneDrive - SmartBear Software, Inc\Documents\TestComplete 15 Projects\Google Search\Google Search.pjs"" /r /p:"Google Search" /t:"KeywordTests|Test4" /exit /SilentMode
