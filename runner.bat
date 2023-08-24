45fc5c15b6f13d623e9597cacaf90ac0684af524
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2023-08-24 01:59:27 +0000

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,3 +1,2 @@
 set ProjectPath="%cd%\Test\Test.pjs"
-
 "C:\PerfLogs\x64\Bin\TestComplete.exe" %ProjectPath% /r /AccessKey:73e7dbca-782b-44d9-8f5a-ee8a297aaeb2 /p:"Test" /t:"KeywordTests|Test1" /exit /SilentMode
