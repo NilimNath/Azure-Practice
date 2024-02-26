c71d515b3c51f3582babecbc49c77ccedfdeacbe
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2024-02-26 12:16:38 +1100

    Using SessionCreator

--- runner.bat
+++ runner.bat
@@ -1,2 +1,3 @@
 set ProjectPath="C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
-"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" %ProjectPath% /r /AccessKey:73e7dbca-782b-44d9-8f5a-ee8a297aaeb2 /p:"TestProject1" /t:"KeywordTests|Test1" /exit
+Rem "C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" %ProjectPath% /r /AccessKey:73e7dbca-782b-44d9-8f5a-ee8a297aaeb2 /p:"TestProject1" /t:"KeywordTests|Test1" /exit
+SessionCreator.exe RunTest /UserName:"Administrator" /Password:"E$Qt*tNDxGh2T1cmRZnVfLAk@CTy3BDs" /UseActiveSession /ProjectPath:%ProjectPath% /project:"TestProject1" /test:"KeywordTests|Test1" /exit
