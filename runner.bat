636bddbdb8bac2363f572ebd39cd5d89a38fc253
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2024-02-26 15:31:32 +1100

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,3 +1,2 @@
-"C:\Users\Administrator\actions-runner\run.cmd"
 set ProjectPath="C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
 "C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\SessionCreator.exe" RunTest /UserName:Administrator /Password:E$Qt*tNDxGh2T1cmRZnVfLAk@CTy3BDs /UA /ScreenResolution:"3440*1440" /ProjectPath:"C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs" /project:"TestProject1" /test:"KeywordTests|Test1"
