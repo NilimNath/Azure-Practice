f548af63bc6db2353e10921a3763cbecd8ab0bd0
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2024-03-05 09:57:37 +1100

    updated path in work machine

--- runner.bat
+++ runner.bat
@@ -1,2 +1,2 @@
-set ProjectPath="C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
+set ProjectPath="C:\Users\nilim.nath\OneDrive - SmartBear Software, Inc\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
 "C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" %ProjectPath% /r /AccessKey:73e7dbca-782b-44d9-8f5a-ee8a297aaeb2 /p:"TestProject1" /t:"KeywordTests|Test1" /exit
