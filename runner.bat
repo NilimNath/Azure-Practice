61dcff655b08cfd8a6d988451d11da043f752a76
Nilim Nath <61951463+NilimNath@users.noreply.github.com>
2024-03-04 22:33:36 +0000

    running in work machine

--- runner.bat
+++ runner.bat
@@ -1,2 +1,2 @@
 set ProjectPath="C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
-"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\SessionCreator.exe" RunTest /UserName:Administrator /Password:E$Qt*tNDxGh2T1cmRZnVfLAk@CTy3BDs /UA /ScreenResolution:"3440*1440" /ProjectPath:"C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs" /project:"TestProject1" /test:"KeywordTests|Test1"
+"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\SessionCreator.exe" RunTest /UserName:nilim.nath /Password:Docklands@sb2024 /UA /ScreenResolution:"3440*1440" /ProjectPath:"C:\Users\Administrator\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs" /project:"TestProject1" /test:"KeywordTests|Test1"
